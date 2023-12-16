c261e1babfbf46f40660e1fb8c369d67c6a57db4
VOsipenkov <jumpViaOcean@gmail.com>
2023-12-17 01:23:05 +0300

    Nuclearbot-17: create current telegram command

--- src/deployments/kafka/stop_k8s.bat
+++ src/deployments/kafka/stop_k8s.bat
@@ -8,7 +8,4 @@ echo step 2..
 kubectl delete pod my-cluster-kafka-0 -n kafka
 timeout 5
 kubectl drain docker-desktop --delete-emptydir-data  --ignore-daemonsets --delete-local-data --force
-timeout 10
-
-echo shutdown system..
-shutdown /s /f /t 0
+timeout 5
