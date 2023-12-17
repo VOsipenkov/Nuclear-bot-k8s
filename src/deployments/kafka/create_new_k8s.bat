bf2955588fbaea8cd459b4fe4b1fbb1aea76752d
VOsipenkov <jumpViaOcean@gmail.com>
2023-12-17 12:08:16 +0300

    Nuclearbot-17: create current telegram command

--- src/deployments/kafka/create_new_k8s.bat
+++ src/deployments/kafka/create_new_k8s.bat
@@ -1,5 +1,5 @@
 @echo OFF
-echo create namespace..
+echo create namespace kafka..
 kubectl create namespace kafka
 
 echo deploy strimzi..
@@ -12,6 +12,22 @@ timeout 60
 
 echo create topics..
 kubectl create -n kafka -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\kafka\Topic.yml
+echo kafka deploy complete
+timeout 2
 
-echo deploy complete
-timeout 10
+echo create namespace postgresql
+kubectl create namespace postgresql
+echo create posgresql..
+kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-persistent-volume.yaml -n postgresql
+timeout 2
+kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-volume-claim.yaml -n postgresql
+timeout 2
+kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-configmap.yaml -n postgresql
+timeout 2
+kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-deployment.yaml -n postgresql
+timeout 2
+kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-service.yaml -n postgresql
+timeout 2
+
+echo postgresql deploy complete
+timeout 2
