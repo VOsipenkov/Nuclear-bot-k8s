302f59b5aedd4f91b83407b2eff847e8f98d5a93
VOsipenkov <jumpViaOcean@gmail.com>
2023-12-23 10:48:25 +0300

    Nuclearbot-17: create current telegram command

--- src/deployments/kafka/create_new_k8s.bat
+++ src/deployments/kafka/create_new_k8s.bat
@@ -1,21 +1,15 @@
 @echo OFF
-echo create namespace kafka..
-kubectl create namespace kafka
 
-echo deploy strimzi..
+echo create kafka..
+kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
 timeout 40
-
-echo deploy kafka..
 kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n kafka
 timeout 60
-
-echo create topics..
 kubectl create -n kafka -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\kafka\Topic.yml
-echo kafka deploy complete
 timeout 2
 
-echo create namespace postgresql
+echo create postgresql
 kubectl create namespace postgresql
 echo create posgresql..
 kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-persistent-volume.yaml -n postgresql
@@ -29,5 +23,18 @@ timeout 2
 kubectl apply -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\postgres\db-service.yaml -n postgresql
 timeout 2
 
-echo postgresql deploy complete
+echo create nuclear-bot
+create namespace nuclear-bot
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\CoreDeployment.yml
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\ParserAgentRadonDeployment.yml
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\ParserAgentUaDeployment.yml
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\TelegramBotDeployment.yml
 timeout 2
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\CoreService.yml
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\ParserAgentRadonService.yml
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\ParserAgentUaService.yml
+kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\TelegramBotService.yml
+
+timeout 2
+echo deploy completed successfully..
+timeout 10
