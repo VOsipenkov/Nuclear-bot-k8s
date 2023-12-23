acd492e3662da94510240ab6b111eb8144fbf3e2
VOsipenkov <jumpViaOcean@gmail.com>
2023-12-23 11:02:00 +0300

    Nuclearbot-17: create current telegram command

--- src/deployments/kafka/create_new_nuclear_bot.bat
+++ src/deployments/kafka/create_new_nuclear_bot.bat
@@ -7,17 +7,14 @@ kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\config\Core-config.ym
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\config\Parser-radon-config.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\config\Parser-ua-config.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\config\Telegram-config.yml
-timeout 2
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\CoreDeployment.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\ParserAgentRadonDeployment.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\ParserAgentUaDeployment.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\TelegramBotDeployment.yml
-timeout 2
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\CoreService.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\ParserAgentRadonService.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\ParserAgentUaService.yml
 kubectl create -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\services\TelegramBotService.yml
 
-timeout 2
 echo deploy completed successfully..
 timeout 10
