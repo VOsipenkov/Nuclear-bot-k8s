77706a29ea377112299d184881199b3068b89d56
VOsipenkov <jumpViaOcean@gmail.com>
2025-03-27 18:49:45 +0300

    Nuclear-bot-23:  Enabled every morning notification

--- src/deployments/kafka/restart_kafka.bat
+++ src/deployments/kafka/restart_kafka.bat
@@ -1,14 +1,11 @@
 @echo OFF
 
-echo create kafka..
+kubectl delete namespace kafka
+timeout 10
 kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
-timeout 40
+timeout 20
 kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n kafka
-timeout 60
-kubectl create -n kafka -f C:\learn\Nuclear-bot\Nuclear-bot-k8s\src\deployments\kafka\Topic.yml
-timeout 2
 
-timeout 2
 echo deploy completed successfully..
 timeout 10
